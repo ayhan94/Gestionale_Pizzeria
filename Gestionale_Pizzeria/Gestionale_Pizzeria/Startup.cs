4bbf3803d990454bfccb8580a9a7a27063a4d316
ayhan94 <ayhan94@hotmail.it>
2015-12-29 09:55:50 +0100

    Progetto_base

---
+++ Gestionale_Pizzeria/Gestionale_Pizzeria/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gestionale_Pizzeria.Startup))]
+namespace Gestionale_Pizzeria
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
